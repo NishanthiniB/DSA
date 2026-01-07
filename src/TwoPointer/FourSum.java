3f7d3bbbdc51c999a4b047396c84d80cbf5c32d7
NishanthiniB <nishasuresh1305@gmail.com>
2026-01-07 07:05:53 +0530

    4 Sum

---
+++ src/TwoPointer/FourSum.java
@@ -0,0 +1,51 @@
+package TwoPointer;
+
+import java.util.*;
+
+public class FourSum {
+
+    static void main() {
+            int[] nums = {1,0,-1,0,-2,2}; int target = 0;
+        System.out.println(fourSum(nums,target));
+
+
+    }
+
+    public static List<List<Integer>> fourSum(int[] nums, int target) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+        int n = nums.length;
+
+        for (int i = 0; i < n - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue; // skip duplicate i
+
+            for (int j = i + 1; j < n - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue; // skip duplicate j
+
+                int left = j + 1;
+                int right = n - 1;
+
+                while (left < right) {
+                    long sum = (long) nums[i] + nums[j] + nums[left] + nums[right];
+
+                    if (sum == target) {
+                        res.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
+
+                        // skip duplicates for left and right
+                        while (left < right && nums[left] == nums[left + 1]) left++;
+                        while (left < right && nums[right] == nums[right - 1]) right--;
+
+                        left++;
+                        right--;
+                    } else if (sum < target) {
+                        left++;
+                    } else {
+                        right--;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+
+}
