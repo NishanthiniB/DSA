59f06e8d76b87395d0d1271098bdb58774d4889c
NishanthiniB <nishasuresh1305@gmail.com>
2026-01-07 07:06:11 +0530

    3 Sum add inputs

--- src/TwoPointer/ThreeSum.java
+++ src/TwoPointer/ThreeSum.java
@@ -5,7 +5,8 @@ import java.util.*;
 public class ThreeSum {
 
     static void main() {
-
+        int[] nums={-1, 0, 1, 2, -1, -4};
+        System.out.println(threeSum(nums));
     }
 
     public static List<List<Integer>> threeSum(int[] nums) {
