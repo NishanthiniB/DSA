21356df6aef6e15ef5414055adbccb673c50a8f4
NishanthiniB <nishasuresh1305@gmail.com>
2026-01-19 14:02:48 +0530

    String Compression

---
+++ src/TwoPointer/StringCompression.java
@@ -0,0 +1,26 @@
+package TwoPointer;
+
+public class StringCompression {
+    static void main() {
+            char[] chars = {'a','a','b','b','c','c','c'};
+        System.out.println(compress(chars));
+
+    }
+
+    public static int compress(char[] chars) {
+         int index=0; int count=0;
+         for(int i=0; i<chars.length; i++){
+             count++;
+             if(i+1==chars.length || chars[i]!=chars[i+1]){
+                 chars[index++]=chars[i];
+                 if(count!=1){
+                     for(char c: Integer.toString(count).toCharArray()){
+                         chars[index++]=c;
+                     }
+                 }
+                 count=0;
+             }
+         }
+         return index;
+    }
+}
