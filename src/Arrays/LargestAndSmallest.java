c0f86035bdc683572f3a797a97426680c907b121
NishanthiniB <nishasuresh1305@gmail.com>
2026-02-24 17:31:30 +0530

    Largest and Smallest in Array

---
+++ src/Arrays/LargestAndSmallest.java
@@ -0,0 +1,28 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class LargestAndSmallest {
+    static void main() {
+    int[] arr = {15,444,300,12,1};
+    int[] result = findLarSmall(arr);
+        System.out.println(Arrays.toString(result));
+    }
+
+    static int[] findLarSmall(int[] arr){
+        int max=arr[0];
+        int min=arr[0];
+        for(int i=1; i<arr.length; i++){
+            if(arr[i]<min){
+                min=arr[i];
+            }
+            else if(arr[i]>max){
+                max=arr[i];
+            }
+        }
+        int[] ans = new int[2];
+        ans[0]=min;
+        ans[1]=max;
+        return ans;
+    }
+}
