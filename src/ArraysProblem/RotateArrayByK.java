5af0530b338aca3ecd9c47a66ce8713116d34218
NishanthiniB <nishasuresh1305@gmail.com>
2026-02-25 07:23:28 +0530

    Rotate Array By K

---
+++ src/ArraysProblem/RotateArrayByK.java
@@ -0,0 +1,32 @@
+package ArraysProblem;
+
+import java.util.Arrays;
+
+public class RotateArrayByK {
+    static void main() {
+        int[] array = {-1};
+        rotate(array,2);
+        System.out.println(Arrays.toString(array));
+    }
+
+    static void rotate(int[] nums, int k){
+
+        int n = nums.length;
+        if (n == 0) return;
+        k = k % n;
+        if (k == 0) return; //
+
+        reverse(nums, 0, nums.length-1);
+        reverse(nums,0, k-1);
+        reverse(nums, k, nums.length-1);
+    }
+
+    static void reverse(int[] arr, int start, int end){
+        while(start<end){
+            int temp=arr[start];
+            arr[start]=arr[end];
+            arr[end]=temp;
+            start++; end--;
+        }
+    }
+}
