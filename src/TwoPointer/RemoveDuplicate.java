494925965dd5382b170ba188b75d43e32edc914d
NishanthiniB <nishasuresh1305@gmail.com>
2026-02-25 14:05:56 +0530

    Remove Duplicate in Array

--- src/TwoPointer/RemoveDuplicate.java
+++ src/TwoPointer/RemoveDuplicate.java
@@ -2,10 +2,19 @@ package TwoPointer;
 
 import javax.xml.transform.Source;
 import java.sql.SQLOutput;
+import java.util.HashSet;
+import java.util.Set;
 
 public class RemoveDuplicate {
     public static void main(String[] args) {
         int[] nums={0,0,1,1,1,2,2,3,3,4};
+
+//        Set<Integer> set = new HashSet<>();
+//        for(int num : nums){
+//            set.add(num);
+//        }
+//        System.out.println(set);
+
         int result = remove(nums);
         for(int i=0; i<result; i++){
             System.out.print(nums[i]+" ");
